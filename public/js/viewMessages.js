b0cde8f1fefbbcf55aca0bdf4c878c3a66993a9a
ethan-kwon <ethan200304@gmail.com>
2021-07-22 18:50:12 +0000

    finish mild and medium goals

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -6,9 +6,24 @@ const getMessages = () => {
         const data = snapshot.val();
         for (let key in data) {
             if (data[key].passcode === passcode.value) {
+                const modal = document.querySelector("#messageModal");
                 const message = document.querySelector("#message");
                 message.innerHTML = data[key].message;
+                modal.classList.add("is-active");
+            } else {
+                const error = document.querySelector("#errorMessage");
+                error.classList.remove("hidden");
             }
         }
     })
+}
+
+const closeModal = () => {
+    const modal = document.querySelector("#messageModal");
+    modal.classList.remove("is-active");
+}
+
+const closeError = () => {
+    const modal = document.querySelector("#errorMessage");
+    modal.classList.add("hidden");
 }
